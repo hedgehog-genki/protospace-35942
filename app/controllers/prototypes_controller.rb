667fe1f6eb615dcb070db8e9a05d263cfcae5686
hedgehog-genki <84561429+hedgehog-genki@users.noreply.github.com>
2021-06-08 12:36:34 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -33,7 +34,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless @prototype_user.id == current_user.id
+    if prototype.user_id != current_user.id
       redirect_to action: :index
     end
   end
@@ -55,5 +56,11 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
